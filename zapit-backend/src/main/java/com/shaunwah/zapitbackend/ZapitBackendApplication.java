ff8cd5162cc740a94a5b7b228e49537f48606a85
Shaun Wah <me@shaunwah.com>
2023-09-04 12:57:45 +0800

    Initial commit

---
+++ zapit-backend/src/main/java/com/shaunwah/zapitbackend/ZapitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shaunwah.zapitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZapitBackendApplication.class, args);
+    }
+
+}
---
+++ zapit-backend/src/test/java/com/shaunwah/zapitbackend/ZapitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaunwah.zapitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
