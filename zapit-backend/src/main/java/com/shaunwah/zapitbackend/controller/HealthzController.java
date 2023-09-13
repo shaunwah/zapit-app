3eaa44b46276bbe13b4a475ee6d5694094940dd7
Shaun Wah <me@shaunwah.com>
2023-09-13 23:35:26 +0800

    Add healthz

--- zapit-backend/src/main/java/com/shaunwah/zapitbackend/controller/HealthzController.java
+++ zapit-backend/src/main/java/com/shaunwah/zapitbackend/controller/HealthzController.java
@@ -1,5 +1,7 @@
 package com.shaunwah.zapitbackend.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.java.Log;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-public class HealthController {
+@Log
+public class HealthzController {
     @GetMapping("/healthz")
-    public ResponseEntity<String> getHealthz() {
+    public ResponseEntity<String> getHealthz(HttpServletRequest request) {
+        log.info("Ping from %s via /healthz!".formatted(request.getRemoteAddr()));
         return ResponseEntity.ok("pong!");
     }
 
     @GetMapping("/api/healthz")
-    public ResponseEntity<String> getApiHealthz() {
+    public ResponseEntity<String> getApiHealthz(HttpServletRequest request) {
+        log.info("Ping from %s via /api/healthz!".formatted(request.getRemoteAddr()));
         return ResponseEntity.ok("pong!");
     }
 }
