e5ffe66a49e9091698a92d4f47e1bcf8db9528fd
Shaun Wah <me@shaunwah.com>
2023-09-14 01:39:00 +0800

    Add stuff

--- zapit-backend/src/main/java/com/shaunwah/zapitbackend/config/WebConfig.java
+++ zapit-backend/src/main/java/com/shaunwah/zapitbackend/config/WebConfig.java
@@ -2,9 +2,12 @@ package com.shaunwah.zapitbackend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
