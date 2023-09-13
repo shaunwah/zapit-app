8b0fde7858f9bee14b63b3393e53b6fca95eb923
Shaun Wah <me@shaunwah.com>
2023-09-14 02:14:21 +0800

    Add stuff

--- zapit-backend/src/main/java/com/shaunwah/zapitbackend/config/WebConfig.java
+++ zapit-backend/src/main/java/com/shaunwah/zapitbackend/config/WebConfig.java
@@ -9,8 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedOrigins("*");
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("https://app-zapit.shaunwah.dev");
+    }
 }
